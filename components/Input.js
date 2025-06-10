7e1e8d82624454177cbd3a0e438d47fad3ffdd74
Dx_craig <steviedoe20@gmail.com>
2025-06-10 20:14:35 +0000

    feat: update Input component to display text label and style input fields

--- components/Input.js
+++ components/Input.js
@@ -1,15 +1,35 @@
-import { TextInput } from "react-native"
+import { StyleSheet, TextInput, View ,Text} from "react-native"
 import React, {useState} from "react"
 
-const Input = () => {
+const Input = (text) => {
     const [fullName, setFullName] = useState('')
     const [phoneNumber, setPhoneNumber] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
-    
+
     return(
-        <TextInput/>
+        <View style = {styles.view}>
+            <Text style ={styles.text}>{text}</Text>
+            <TextInput style ={styles.input}/>
+        </View>
+        
     )
 }
 
 export default Input
+
+const styles = StyleSheet.create({
+    input: {
+    height: 59,
+    backgroundColor: '#E8DEFE',
+    borderRadius: 8,
+    paddingLeft: 20
+  },
+  view: {
+    marginBottom: 20,
+    gap: 10
+  },
+   text: {
+    color: '#11042F'
+  }
+})
